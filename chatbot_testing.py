f3e40d5583caf3031a6fc3526f7c6a96bd38cd5d
Kyli McQueen <36653302+kylimcqueen@users.noreply.github.com>
2025-02-11 18:47:03 -0700

    Unable to correctly import dotenv, hoping that restarting machine helps.

--- chatbot_testing.py
+++ chatbot_testing.py
@@ -1,6 +1,7 @@
 import os
+from openai import OpenAI
 from langchain_openai import OpenAIEmbeddings
 from dotenv import load_dotenv
-
-
 load_dotenv()
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
