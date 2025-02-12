0fd65c99e9118e5e3019f813d5b33fe8842a7a4c
Kyli McQueen <36653302+kylimcqueen@users.noreply.github.com>
2025-02-11 19:45:36 -0700

    Update chatbot_testing to include pypdfloader.

--- chatbot_testing.py
+++ chatbot_testing.py
@@ -1,7 +1,14 @@
 import os
 from openai import OpenAI
 from langchain_openai import OpenAIEmbeddings
-from dotenv import load_dotenv
-load_dotenv()
+from langchain_community.document_loaders import PyPDFLoader
+
+
+#manually export the keys because dotenv is satan
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
+huggingface_api_key = os.getenv("HUGGINGFACE_API_KEY")
+langchain_api_key = os.getenv("LANGCHAIN_API_KEY")
+
+#Load and preprocess documents
+loader = PyPDFLoader("/Users/kyli/Desktop/lab_gpt_example_docs/01_pathophys_tbi.pdf")
