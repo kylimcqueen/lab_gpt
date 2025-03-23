4f7e685eb93b71e0b406af37025eab0ada7a97bb
Kyli McQueen <36653302+kylimcqueen@users.noreply.github.com>
2025-03-23 13:41:55 -0700

    Deletions.

--- chatbot_testing.py
+++ chatbot_testing.py
@@ -60,18 +60,3 @@ collection.add(
 )
 
 
-chat = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0.2)
-
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-
-prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            "You are a helpful assistant. Answer all questions to the best of your ability.",
-        ),
-        MessagesPlaceholder(variable_name="messages"),
-    ]
-)
-
-chain = prompt | chat
