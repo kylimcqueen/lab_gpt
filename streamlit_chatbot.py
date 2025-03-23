0b56a4a663c70d3aa06a388b42ff94d82f7e172a
Kyli McQueen <36653302+kylimcqueen@users.noreply.github.com>
2025-03-23 13:42:08 -0700

    Create streamlit_chatbot.py to follow tutorial.

---
+++ streamlit_chatbot.py
@@ -0,0 +1,11 @@
+'''
+Streamlit Langchain tutorial on YT
+Link: https://www.youtube.com/watch?v=zKGeRWjJlTU
+'''
+
+import streamlit as st
+from dotenv import load_dotenv
+
+load_dotenv()
+
+st.set_page_config(page_title="Streaming Bot", page_icon="s")
